ef036fb61f652a70f615075be9e9bd9468e13fb4
EliseyAg <elisey.tagan@gmail.com>
2025-10-31 18:38:44 +0300

    Basic Flask script

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/messenger')
+def messenger():
+    return 'Messenger'
+
+
 if __name__ == '__main__':
-    print("Hello world!")
+    app.run(host='0.0.0.0')
